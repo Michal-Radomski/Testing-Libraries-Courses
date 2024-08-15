a67929ea20c20876f8135fe1f58ecc4ffcc66c78
Michal Radomski <michalra@op.pl>
2024-08-15 12:13:03 +0200

    React Testing Library - commit 21

--- 01_React-Testing-Library_Jest/03/src/App.tsx
+++ 01_React-Testing-Library_Jest/03/src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 import "./App.scss";
 import RoleExample from "./components/RoleExample";
+import AccessibleName from "./components/AccessibleName";
 
 const App = (): JSX.Element => {
   return (
     <React.Fragment>
       <RoleExample />
+      <br />
+      <hr />
+      <AccessibleName />
       <br />
       <hr />
     </React.Fragment>
---
+++ 01_React-Testing-Library_Jest/03/src/components/AccessibleName.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AccessibleName = (): JSX.Element => {
+  return (
+    <React.Fragment>
+      <div>
+        <button>Submit</button>
+        <button>Cancel</button>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default AccessibleName;
