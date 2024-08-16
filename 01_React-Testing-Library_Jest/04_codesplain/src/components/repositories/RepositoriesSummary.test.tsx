687243b2d263b683e1014ea83da04188acdb04ac
Michal Radomski <michalra@op.pl>
2024-08-17 00:46:59 +0200

    React Testing Library - commit 32

--- 01_React-Testing-Library_Jest/04_codesplain/src/components/repositories/RepositoriesSummary.test.tsx
+++ 01_React-Testing-Library_Jest/04_codesplain/src/components/repositories/RepositoriesSummary.test.tsx
@@ -2,8 +2,15 @@ import { screen, render } from "@testing-library/react";
 
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("displays information about the repository", () => {
-  const repository = {
+interface Repository {
+  language: string;
+  stargazers_count: number;
+  forks: number;
+  open_issues: number;
+}
+
+test("displays information about the repository", (): void => {
+  const repository: Repository = {
     language: "Javascript",
     stargazers_count: 5,
     forks: 30,
@@ -12,8 +19,10 @@ test("displays information about the repository", () => {
   render(<RepositoriesSummary repository={repository} />);
 
   for (let key in repository) {
-    const value = repository[key as keyof typeof repository];
-    const element = screen.getByText(new RegExp(String(value)));
+    // const value = repository[key as keyof typeof repository] as string | number;
+    const value = repository[key as keyof Repository] as string | number;
+    const element = screen.getByText(new RegExp(String(value))) as HTMLDivElement;
+    // console.log("element:", element);
 
     expect(element).toBeInTheDocument();
   }
