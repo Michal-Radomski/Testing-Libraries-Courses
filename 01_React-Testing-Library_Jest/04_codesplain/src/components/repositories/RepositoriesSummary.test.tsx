1206a232b0f2aa64a269e317420708f77533bc4f
Michal Radomski <michalra@op.pl>
2024-08-18 19:04:51 +0200

    React Testing Library - commit 41

--- 01_React-Testing-Library_Jest/04_codesplain/src/components/repositories/RepositoriesSummary.test.tsx
+++ 01_React-Testing-Library_Jest/04_codesplain/src/components/repositories/RepositoriesSummary.test.tsx
@@ -2,7 +2,26 @@ import { screen, render } from "@testing-library/react";
 
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("displays information about the repository", (): void => {
+test("displays information about the repository - V1 - test", (): void => {
+  const repository: Repository = {
+    language: "Javascript",
+    stargazers_count: 5,
+    forks: 30,
+    open_issues: 1,
+  };
+  render(<RepositoriesSummary repository={repository} />);
+
+  for (let key in repository) {
+    // const value = repository[key as keyof typeof repository] as string | number;
+    const value = repository[key as keyof Repository] as string | number;
+    const element = screen.getByText(new RegExp(String(value))) as HTMLDivElement;
+    // console.log("element:", element);
+
+    expect(element).toBeInTheDocument();
+  }
+});
+
+it("displays information about the repository - V2 - it", (): void => {
   const repository: Repository = {
     language: "Javascript",
     stargazers_count: 5,
