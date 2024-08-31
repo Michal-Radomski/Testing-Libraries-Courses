d1475146f48534f2167fed6b927ff52ed62ec19e
Michal Radomski <michalra@op.pl>
2024-09-01 00:22:37 +0200

    Selenium with JS and Mocha course - commit 04

--- 02_Selenium_Mocha/test1.ts
+++ 02_Selenium_Mocha/test1.ts
@@ -8,21 +8,27 @@ const pause = (time: number): Promise<number> => new Promise((resolve) => setTim
   // console.log("driver:", driver);
 
   try {
-    await driver.get("https://www.google.com/ncr");
-    await pause(2500);
-    await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
-    await pause(2500);
-    const results: WebElement = await driver.wait(until.titleIs("webdriver - Google Search"), 10000);
-    console.log("results:", results);
-    await pause(2500);
+    // await driver.get("https://www.google.com/ncr");
+    // await pause(2500);
+    // await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
+    // await pause(2500);
+    // const results: WebElement = await driver.wait(until.titleIs("webdriver - Google Search"), 10000);
+    // console.log("results:", results);
+    // await pause(2500);
 
-    await driver.navigate().to("https:onet.pl");
+    await driver.navigate().to("https://www.onet.pl");
     await driver.sleep(2500);
     await driver.navigate().back();
     await driver.sleep(2500);
     await driver.navigate().forward();
     await driver.sleep(2500);
     await driver.navigate().refresh();
+    await driver.manage().window().maximize();
+    await driver.manage().window().minimize();
+    await driver.manage().window().fullscreen();
+    await driver.manage().window().setSize(200, 100);
+    const windowSize = await driver.manage().window().getSize();
+    console.log("windowSize:", windowSize);
     await driver.sleep(2500);
   } catch (error) {
     console.log("error:", error);
