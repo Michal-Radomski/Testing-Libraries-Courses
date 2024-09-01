d1d532a7272e5f32387850717456b6473252bc55
Michal Radomski <michalra@op.pl>
2024-09-01 19:43:12 +0200

    Selenium with JS and Mocha course - commit 16

--- 02_Selenium_Mocha/test/test3.mocha.spec.ts
+++ 02_Selenium_Mocha/test/test3.mocha.spec.ts
@@ -1,5 +1,6 @@
 import assert from "assert";
-import { WebDriver, By, Builder, WebElement, WebElementPromise, Actions, IRectangle } from "selenium-webdriver";
+import { platform } from "process";
+import { WebDriver, By, Builder, WebElement, WebElementPromise, Actions, IRectangle, Key } from "selenium-webdriver";
 
 //* Actions - Drag and Drop
 describe("Drag and Drop", function (): void {
@@ -54,5 +55,83 @@ describe("Double click", function (): void {
     await driver.sleep(500);
     const status: string = await driver.findElement(By.id("click-status")).getText();
     assert.deepStrictEqual(status, `double-clicked`);
+  });
+});
+
+//* Actions - Keys Test
+describe("Keyboard Action - Keys test", function (): void {
+  let driver: WebDriver;
+
+  before(async function (): Promise<void> {
+    driver = await new Builder().forBrowser("firefox").build();
+  });
+
+  after((): Promise<void> => driver.quit());
+
+  it("KeyDown", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/single_text_input.html");
+
+    await driver.actions().keyDown(Key.SHIFT).sendKeys("a").perform();
+
+    const textField: WebElementPromise = driver.findElement(By.id("textInput"));
+    assert.deepStrictEqual(await textField.getAttribute("value"), "A");
+  });
+
+  it("KeyUp", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/single_text_input.html");
+
+    const textField: WebElementPromise = driver.findElement(By.id("textInput"));
+    await textField.click();
+
+    await driver.actions().keyDown(Key.SHIFT).sendKeys("a").keyUp(Key.SHIFT).sendKeys("b").perform();
+
+    assert.deepStrictEqual(await textField.getAttribute("value"), "Ab");
+  });
+
+  it("sendKeys", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/single_text_input.html");
+
+    const textField: WebElementPromise = driver.findElement(By.id("textInput"));
+    await textField.click();
+
+    await driver.actions().sendKeys("abc").perform();
+
+    assert.deepStrictEqual(await textField.getAttribute("value"), "abc");
+  });
+
+  it("Designated Element", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/single_text_input.html");
+
+    await driver.findElement(By.css("body")).click();
+    const textField: WebElement = await driver.findElement(By.id("textInput"));
+    await driver
+      .actions()
+      .sendKeys(textField as unknown as string, "abc" as string)
+      .perform();
+
+    assert.deepStrictEqual(await textField.getAttribute("value"), "abc");
+  });
+
+  it("Copy and Paste", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/single_text_input.html");
+
+    const textField: WebElement = await driver.findElement(By.id("textInput"));
+
+    const cmdCtrl: string = platform.includes("darwin") ? Key.COMMAND : Key.CONTROL;
+
+    await driver
+      .actions()
+      .click(textField)
+      .sendKeys("Selenium!")
+      .sendKeys(Key.ARROW_LEFT)
+      .keyDown(Key.SHIFT)
+      .sendKeys(Key.ARROW_UP)
+      .keyUp(Key.SHIFT)
+      .keyDown(cmdCtrl)
+      .sendKeys("xvv")
+      .keyUp(cmdCtrl)
+      .perform();
+
+    assert.deepStrictEqual(await textField.getAttribute("value"), "SeleniumSelenium!");
   });
 });
