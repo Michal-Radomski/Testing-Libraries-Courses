44538292e2b21c7e275ad532581dcfb9c5659252
Michal Radomski <michalra@op.pl>
2024-09-01 14:29:30 +0200

    Selenium with JS and Mocha course - commit 10

---
+++ 02_Selenium_Mocha/test/test.chai.spec.ts
@@ -0,0 +1,30 @@
+import { Builder, By, Key, until, WebDriver } from "selenium-webdriver";
+import { expect } from "chai";
+
+describe("Google Search", function () {
+  let driver: WebDriver;
+
+  before(async function () {
+    driver = await new Builder().forBrowser("firefox").build();
+  });
+
+  after(async function () {
+    await driver.quit();
+  });
+
+  it("should display the correct title", async function () {
+    await driver.get("https://www.google.com");
+    await driver.sleep(2500);
+    const title = await driver.getTitle();
+    expect(title).to.equal("Google");
+  });
+
+  it("should search for Selenium", async function () {
+    await driver.get("https://www.google.com");
+    await driver.sleep(2500);
+    await driver.findElement(By.name("q")).sendKeys("Selenium WebDriver", Key.RETURN);
+    await driver.wait(until.titleContains("Selenium WebDriver"), 1000);
+    const title = await driver.getTitle();
+    expect(title).to.include("Selenium WebDriver");
+  });
+});
