cb7fe437589929c13df6dcfa6e8805a1563e6c71
Michal Radomski <michalra@op.pl>
2024-09-03 23:45:20 +0200

    Selenium with JS and Mocha course - commit 25 - additional exercise

---
+++ 02_Selenium_Mocha/test/test8.mocha_.spec.ts
@@ -0,0 +1,163 @@
+import assert from "assert";
+import { Browser, Builder, By, ThenableWebDriver, WebDriver, WebElement } from "selenium-webdriver";
+import Firefox from "selenium-webdriver/firefox";
+
+//* 1
+describe("Page loading strategies", function (): void {
+  const options = new Firefox.Options();
+
+  it("Navigate using eager page loading strategy", async function (): Promise<void> {
+    const driver: ThenableWebDriver = new Builder()
+      .forBrowser(Browser.FIREFOX)
+      // @ts-ignore
+      .setFirefoxOptions(options.setPageLoadStrategy("eager"))
+      .build();
+
+    await driver.get("https://www.selenium.dev/selenium/web/blank.html");
+    await driver.quit();
+  });
+
+  it("Navigate using none page loading strategy", async function (): Promise<void> {
+    const driver: ThenableWebDriver = new Builder()
+      .forBrowser(Browser.FIREFOX)
+      // @ts-ignore
+      .setFirefoxOptions(options.setPageLoadStrategy("none"))
+      .build();
+
+    await driver.get("https://www.selenium.dev/selenium/web/blank.html");
+    await driver.quit();
+  });
+
+  it("Navigate using normal page loading strategy", async function (): Promise<void> {
+    const driver: ThenableWebDriver = new Builder()
+      .forBrowser(Browser.FIREFOX)
+      // @ts-ignore
+      .setFirefoxOptions(options.setPageLoadStrategy("normal"))
+      .build();
+
+    await driver.get("https://www.selenium.dev/selenium/web/blank.html");
+    await driver.quit();
+  });
+
+  it("Should be able to accept certs", async function (): Promise<void> {
+    const driver: ThenableWebDriver = new Builder()
+      .forBrowser(Browser.FIREFOX)
+      // @ts-ignore
+      .setFirefoxOptions(options.setAcceptInsecureCerts(true))
+      .build();
+
+    await driver.get("https://www.selenium.dev/selenium/web/blank.html");
+    await driver.quit();
+  });
+});
+
+//* 2.
+describe("Element Interactions", function (): void {
+  let driver: WebDriver;
+
+  before(async function (): Promise<void> {
+    driver = new Builder().forBrowser(Browser.FIREFOX).build();
+  });
+
+  after(async (): Promise<void> => await driver.quit());
+
+  it("should Clear input and send keys into input field", async function (): Promise<void> {
+    try {
+      await driver.get("https://www.selenium.dev/selenium/web/inputs.html");
+      const inputField: WebElement = await driver.findElement(By.name("no_type"));
+      await inputField.clear();
+      await inputField.sendKeys("Selenium");
+      const text: string = await inputField.getAttribute("value");
+      assert.strictEqual(text, "Selenium");
+    } catch (error) {
+      console.log("error:", error);
+    }
+  });
+});
+
+//* 3.
+describe("Cookies", function (): void {
+  let driver: WebDriver;
+
+  before(async function (): Promise<void> {
+    driver = new Builder().forBrowser(Browser.FIREFOX).build();
+  });
+
+  after(async (): Promise<void> => await driver.quit());
+
+  it("Create a cookie", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/blank.html");
+
+    // set a cookie on the current domain
+    await driver.manage().addCookie({ name: "key", value: "value" });
+  });
+
+  it("Create cookies with sameSite", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/blank.html");
+
+    // set a cookie on the current domain with sameSite 'Strict' (or) 'Lax'
+    await driver.manage().addCookie({ name: "key", value: "value", sameSite: "Strict" });
+    await driver.manage().addCookie({ name: "key", value: "value", sameSite: "Lax" });
+  });
+
+  it("Read cookie", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/blank.html");
+
+    // set a cookie on the current domain
+    await driver.manage().addCookie({ name: "foo", value: "bar" });
+
+    // Get cookie details with named cookie 'foo'
+    await driver
+      .manage()
+      .getCookie("foo")
+      .then(function (cookie) {
+        console.log("cookie details => ", cookie);
+      });
+  });
+
+  it("Read all cookies", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/blank.html");
+
+    // Add few cookies
+    await driver.manage().addCookie({ name: "test1", value: "cookie1" });
+    await driver.manage().addCookie({ name: "test2", value: "cookie2" });
+
+    // Get all Available cookies
+    await driver
+      .manage()
+      .getCookies()
+      .then(function (cookies) {
+        console.log("cookie details => ", cookies);
+      });
+  });
+
+  it("Delete a cookie", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/blank.html");
+
+    // Add few cookies
+    await driver.manage().addCookie({ name: "test1", value: "cookie1" });
+    await driver.manage().addCookie({ name: "test2", value: "cookie2" });
+
+    // Delete a cookie with name 'test1'
+    await driver.manage().deleteCookie("test1");
+
+    // Get all Available cookies
+    await driver
+      .manage()
+      .getCookies()
+      .then(function (cookies) {
+        console.log("cookie details => ", cookies);
+      });
+  });
+
+  it("Delete all cookies", async function (): Promise<void> {
+    await driver.get("https://www.selenium.dev/selenium/web/blank.html");
+
+    // Add few cookies
+    await driver.manage().addCookie({ name: "test1", value: "cookie1" });
+    await driver.manage().addCookie({ name: "test2", value: "cookie2" });
+
+    // Delete all cookies
+    await driver.manage().deleteAllCookies();
+  });
+});
