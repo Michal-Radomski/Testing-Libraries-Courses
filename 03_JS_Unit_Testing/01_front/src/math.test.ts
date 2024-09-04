db027c373191c75037c874b5f2d5678dae3dd997
Michal Radomski <michalra@op.pl>
2024-09-05 00:42:26 +0200

    JS Unit Testing course - commit 06

--- 03_JS_Unit_Testing/01_front/src/math.test.ts
+++ 03_JS_Unit_Testing/01_front/src/math.test.ts
@@ -3,12 +3,23 @@ import { it, expect, test } from "vitest";
 import { add } from "./math";
 
 //* it = test in Jest and Vitest
-it("should summarize all number values in an array", (): void => {
-  const result = add([1, 2, 3, 4, 5]) as number;
-  expect(result).toBe(15);
+it("should summarize all number values in an array v1", (): void => {
+  const result = add([1, 2, 3, 4]) as number;
+  expect(result).toBe(10);
 });
 
-test("should summarize all number values in an array", (): void => {
-  const result = add([1, 2, 3, 4, 5]) as number;
-  expect(result).toBe(15);
+test("should summarize all number values in an array v2", (): void => {
+  const result = add([1, 2, 3, 4]) as number;
+  expect(result).toBe(10);
+});
+
+//* AAA Pattern
+it("should summarize all number values in an array v3", (): void => {
+  // Arrange
+  const numbers = [1, 2, 3, 4];
+  // Act
+  const result: number = add(numbers);
+  // Assert
+  const expectedResult: number = numbers.reduce((prevValue, curValue) => prevValue + curValue, 0);
+  expect(result).toBe(expectedResult);
 });
