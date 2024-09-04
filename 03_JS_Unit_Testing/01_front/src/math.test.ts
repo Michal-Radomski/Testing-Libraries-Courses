c95cb8d015f8dbf0d5f5cd87ed695ea7c4e039d1
Michal Radomski <michalra@op.pl>
2024-09-05 00:31:12 +0200

    JS Unit Testing course - commit 05

--- 03_JS_Unit_Testing/01_front/src/math.test.ts
+++ 03_JS_Unit_Testing/01_front/src/math.test.ts
@@ -1,8 +1,14 @@
-import { it, expect } from "vitest";
+import { it, expect, test } from "vitest";
 
 import { add } from "./math";
 
+//* it = test in Jest and Vitest
 it("should summarize all number values in an array", (): void => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  const result = add([1, 2, 3, 4, 5]) as number;
+  expect(result).toBe(15);
+});
+
+test("should summarize all number values in an array", (): void => {
+  const result = add([1, 2, 3, 4, 5]) as number;
+  expect(result).toBe(15);
 });
