6a805275bdbcde461166120e37c227d1ba85f3ca
Michal Radomski <michalra@op.pl>
2024-09-06 00:52:48 +0200

    JS Unit Testing course - commit 07

--- 03_JS_Unit_Testing/01_front/src/math.test.ts
+++ 03_JS_Unit_Testing/01_front/src/math.test.ts
@@ -23,3 +23,46 @@ it("should summarize all number values in an array v3", (): void => {
   const expectedResult: number = numbers.reduce((prevValue, curValue) => prevValue + curValue, 0);
   expect(result).toBe(expectedResult);
 });
+
+it("should yield NaN if a least one invalid number is provided", (): void => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", (): void => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce((prevValue, curValue) => +prevValue + +curValue, 0);
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield 0 if an empty array is provided", (): void => {
+  const numbers = [] as number[];
+
+  const result: number = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+it("should throw an error if no value is passed into the function", (): void => {
+  const resultFn = (): void => {
+    add();
+  };
+  expect(resultFn).toThrow(/is not iterable/);
+});
+
+it("should throw an error if provided with multiple arguments instead of an array", (): void => {
+  const num1 = 1;
+  const num2 = 2;
+
+  const resultFn = (): void => {
+    add(num1, num2);
+  };
+
+  expect(resultFn).toThrow(/is not iterable/);
+});
