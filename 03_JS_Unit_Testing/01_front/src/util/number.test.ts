1d783565bf5b9d60393ea71dfdbc03a912d4e4dc
Michal Radomski <michalra@op.pl>
2024-09-06 01:24:10 +0200

    JS Unit Testing course - commit 09

---
+++ 03_JS_Unit_Testing/01_front/src/util/number.test.ts
@@ -0,0 +1,34 @@
+import { it, expect } from "vitest";
+
+import { transformToNumber } from "./numbers";
+
+// it("should transform a string number to a number of type number", (): void => {
+//   const input = "1";
+
+//   const result = transformToNumber(input);
+
+//   expect(result).toBeTypeOf("number")
+//   expect(result).toBe(1);
+//   expect(result).toEqual(1);
+//   expect(result).toStrictEqual(1);
+// });
+
+it("should transform a string number to a number of type number", (): void => {
+  const input = "1";
+
+  const result = transformToNumber(input);
+
+  expect(result).toBe(+input);
+  expect(result).toBe(1);
+});
+
+it("should yield NaN for non-transformable values", (): void => {
+  const input = "invalid";
+  const input2 = {};
+
+  const result = transformToNumber(input);
+  const result2 = transformToNumber(input2);
+
+  expect(result).toBeNaN();
+  expect(result2).toBeNaN();
+});
