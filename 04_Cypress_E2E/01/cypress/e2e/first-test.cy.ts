cf5a91b21461fa0b17c3ba409f3d2eed278caee5
Michal Radomski <michalra@op.pl>
2024-09-08 00:06:58 +0200

    Cypress End-to-End Testing course - commit 04

--- 04_Cypress_E2E/01/cypress/e2e/first-test.cy.ts
+++ 04_Cypress_E2E/01/cypress/e2e/first-test.cy.ts
@@ -1,10 +1,10 @@
-// describe("template spec", () => {
+// describe("template spec", (): void => {
 //   it("passes", () => {
 //     cy.visit("https://example.cypress.io");
 //   });
 // });
 
-describe("template spec", () => {
+describe("template spec", (): void => {
   it("passes", () => {
     cy.visit("http://localhost:5173/");
     cy.get("li").should("have.length", 6);
