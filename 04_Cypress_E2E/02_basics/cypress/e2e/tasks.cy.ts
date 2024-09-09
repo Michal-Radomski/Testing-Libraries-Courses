8b24aa816a25dc64634e33b79286e8c8378c9d23
Michal Radomski <michalra@op.pl>
2024-09-10 00:41:09 +0200

    Cypress End-to-End Testing course - commit 10

--- 04_Cypress_E2E/02_basics/cypress/e2e/tasks.cy.ts
+++ 04_Cypress_E2E/02_basics/cypress/e2e/tasks.cy.ts
@@ -23,7 +23,48 @@ describe("tasks management", (): void => {
     cy.get(".backdrop").should("not.exist");
     cy.get(".modal").should("not.exist");
     cy.get(".task").should("have.length", 1);
+    cy.get(".task").should("exist");
     cy.get(".task h2").contains("New Task");
     cy.get(".task p").contains("Some description");
+  });
+
+  it("should validate user input", (): void => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get(".modal").contains("Add Task").click();
+    cy.contains("Please provide values");
+  });
+
+  it("should filter tasks", (): void => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("New Task");
+    cy.get("#summary").type("Some description");
+    cy.get("#category").select("urgent");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("moderate");
+    cy.get(".task").should("have.length", 0);
+    cy.get("#filter").select("urgent");
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("all");
+    cy.get(".task").should("have.length", 1);
+  });
+
+  it("should add multiple tasks", (): void => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("Task 1");
+    cy.get("#summary").type("First task");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+
+    cy.contains("Add Task").click();
+    cy.get("#title").type("Task 2");
+    cy.get("#summary").type("Second task");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 2);
+    cy.get(".task").eq(0).contains("First task"); // first()
+    cy.get(".task").eq(1).contains("Second task"); // last()
   });
 });
