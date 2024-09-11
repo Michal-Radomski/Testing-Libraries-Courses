718a800c0e4b4217bccb809c53979606b550ac4b
Michal Radomski <michalra@op.pl>
2024-09-12 01:58:43 +0200

    Cypress End-to-End Testing course - commit 14

--- 04_Cypress_E2E/03_deeper/cypress/e2e/navigation.cy.ts
+++ 04_Cypress_E2E/03_deeper/cypress/e2e/navigation.cy.ts
@@ -4,11 +4,13 @@ describe("page navigation", (): void => {
   it("should navigate between pages", (): void => {
     cy.visit("http://localhost:5173/");
     cy.get('[data-cy="header-about-link"]').click();
-    cy.location("pathname").should("eq", "/about"); // /about => About page
+    cy.location("pathname").should("eq", "/about"); //* /about => About page
     cy.go("back");
-    cy.location("pathname").should("eq", "/"); // / => Home page
+    cy.go("forward");
+    cy.go("back");
+    cy.location("pathname").should("eq", "/"); //* / => Home page
     cy.get('[data-cy="header-about-link"]').click();
     cy.get('[data-cy="header-home-link"]').click();
-    cy.location("pathname").should("eq", "/"); // / => Home page
+    cy.location("pathname").should("eq", "/"); //* / => Home page
   });
 });
