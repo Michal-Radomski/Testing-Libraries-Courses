fdd09f4c123240d69938a7cd75ffd1ff3d99a618
Michal Radomski <michalra@op.pl>
2024-09-12 01:44:13 +0200

    Cypress End-to-End Testing course - commit 12

--- 04_Cypress_E2E/03_deeper/src/Interfaces.d.ts
+++ 04_Cypress_E2E/03_deeper/src/Interfaces.d.ts
@@ -2,3 +2,17 @@ declare module "*.scss" {
   const content: { [className: string]: string };
   export = content;
 }
+
+interface RootSubState {
+  value: string;
+  blurred: boolean;
+}
+interface RootState {
+  [key: string]: RootSubState;
+}
+
+interface Action {
+  type: string;
+  input: string | number;
+  value?: string;
+}
--- 04_Cypress_E2E/03_deeper/src/components/ContactForm.tsx
+++ 04_Cypress_E2E/03_deeper/src/components/ContactForm.tsx
@@ -2,20 +2,6 @@ import React from "react";
 
 import classes from "./ContactForm.module.scss";
 
-interface RootSubState {
-  value: string;
-  blurred: boolean;
-}
-interface RootState {
-  [key: string]: RootSubState;
-}
-
-interface Action {
-  type: string;
-  input: string | number;
-  value?: string;
-}
-
 const initialState = {
   name: {
     value: "",
