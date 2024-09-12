26ec35d909e7bc09800a202c7087e67ea31c7bde
Michal Radomski <michalra@op.pl>
2024-09-13 01:36:08 +0200

    Cypress End-to-End Testing course - commit 20

--- 04_Cypress_E2E/03_deeper/cypress/e2e/contact.cy.ts
+++ 04_Cypress_E2E/03_deeper/cypress/e2e/contact.cy.ts
@@ -87,22 +87,25 @@ describe("contact form", (): void => {
     cy.get("@msgInput").focus().blur();
     cy.get("@msgInput")
       .parent()
-      .then((el) => {
-        expect(el.attr("class")).to.contains("invalid");
+      .should((el) => {
+        expect(el.attr("class")).not.to.be.undefined;
+        expect(el.attr("class")).contains("invalid");
       });
 
     cy.get('[data-cy="contact-input-name"]').focus().blur();
     cy.get('[data-cy="contact-input-name"]')
       .parent()
-      .then((el) => {
-        expect(el.attr("class")).to.contains("invalid");
+      .should((el) => {
+        expect(el.attr("class")).not.to.be.undefined;
+        expect(el.attr("class")).contains("invalid");
       });
 
     cy.get('[data-cy="contact-input-email"]').focus().blur();
     cy.get('[data-cy="contact-input-email"]')
       .parent()
-      .then((el) => {
-        expect(el.attr("class")).to.contains("invalid");
+      .should((el) => {
+        expect(el.attr("class")).not.to.be.undefined;
+        expect(el.attr("class")).contains("invalid");
       });
   });
 });
