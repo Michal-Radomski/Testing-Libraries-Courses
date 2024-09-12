fe3aadc90c18b5eadf5c2330e8a6b337741c0094
Michal Radomski <michalra@op.pl>
2024-09-13 01:33:38 +0200

    Cypress End-to-End Testing course - commit 19

--- 04_Cypress_E2E/03_deeper/cypress/e2e/contact.cy.ts
+++ 04_Cypress_E2E/03_deeper/cypress/e2e/contact.cy.ts
@@ -55,8 +55,8 @@
 // });
 
 //* V4
-describe("contact form", () => {
-  it("should submit the form", () => {
+describe("contact form", (): void => {
+  it("should submit the form", (): void => {
     cy.visit("http://localhost:5173/about");
     cy.get('[data-cy="contact-input-message"]').type("Hello world!");
     cy.get('[data-cy="contact-input-name"]').type("John Doe");
@@ -72,5 +72,37 @@ describe("contact form", () => {
     // cy.get('@submitBtn').click();
     cy.get("@submitBtn").contains("Sending...");
     cy.get("@submitBtn").should("have.attr", "disabled");
+  });
+
+  //* New test
+  it("should validate the form input", (): void => {
+    cy.visit("http://localhost:5173/about");
+    cy.get('[data-cy="contact-btn-submit"]').click();
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      expect(el).to.not.have.attr("disabled");
+      expect(el.text()).to.not.equal("Sending...");
+    });
+    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
+    cy.get('[data-cy="contact-input-message"]').as("msgInput");
+    cy.get("@msgInput").focus().blur();
+    cy.get("@msgInput")
+      .parent()
+      .then((el) => {
+        expect(el.attr("class")).to.contains("invalid");
+      });
+
+    cy.get('[data-cy="contact-input-name"]').focus().blur();
+    cy.get('[data-cy="contact-input-name"]')
+      .parent()
+      .then((el) => {
+        expect(el.attr("class")).to.contains("invalid");
+      });
+
+    cy.get('[data-cy="contact-input-email"]').focus().blur();
+    cy.get('[data-cy="contact-input-email"]')
+      .parent()
+      .then((el) => {
+        expect(el.attr("class")).to.contains("invalid");
+      });
   });
 });
