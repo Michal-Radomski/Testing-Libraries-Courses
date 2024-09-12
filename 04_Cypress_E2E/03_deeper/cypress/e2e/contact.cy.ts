ae13b63c657e263f013555ad2f655f0a5e062a61
Michal Radomski <michalra@op.pl>
2024-09-13 01:03:34 +0200

    Cypress End-to-End Testing course - commit 18

--- 04_Cypress_E2E/03_deeper/cypress/e2e/contact.cy.ts
+++ 04_Cypress_E2E/03_deeper/cypress/e2e/contact.cy.ts
@@ -28,26 +28,48 @@
 //     cy.get("@submitBtn").contains("Sending...");
 //     cy.get("@submitBtn").should("have.attr", "disabled");
 //     // const btn:Cypress.Chainable<JQuery<HTMLElement>> = cy.get('[data-cy="contact-btn-submit"]'); //* Not recommended!
-//     // console.log("btn:", btn);
+//     // cy.log("btn:", btn);
 //   });
 // });
 
 //* V3
-describe("contact form", (): void => {
-  it("should submit the form", (): void => {
+// describe("contact form", (): void => {
+//   it("should submit the form", (): void => {
+//     cy.visit("http://localhost:5173/about");
+//     cy.get('[data-cy="contact-input-message"]').type("Hello world!");
+//     cy.get('[data-cy="contact-input-name"]').type("John Doe");
+//     cy.get('[data-cy="contact-input-email"]').type("test@example.com");
+//     cy.get('[data-cy="contact-btn-submit"]').then((el: JQuery<HTMLElement>) => {
+//       // cy.log("el:", el);
+//       expect(el.attr("disabled")).to.be.undefined;
+//       expect(el.text()).to.eq("Send Message");
+//     });
+//     // cy.get('[data-cy="contact-btn-submit"]')
+//     //   .contains('Send Message')
+//     //   .should('not.have.attr', 'disabled');
+//     cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
+//     cy.get("@submitBtn").click();
+//     cy.get("@submitBtn").contains("Sending...");
+//     cy.get("@submitBtn").should("have.attr", "disabled");
+//   });
+// });
+
+//* V4
+describe("contact form", () => {
+  it("should submit the form", () => {
     cy.visit("http://localhost:5173/about");
     cy.get('[data-cy="contact-input-message"]').type("Hello world!");
     cy.get('[data-cy="contact-input-name"]').type("John Doe");
-    cy.get('[data-cy="contact-input-email"]').type("test@example.com");
-    cy.get('[data-cy="contact-btn-submit"]').then((el: JQuery<HTMLElement>) => {
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
       expect(el.attr("disabled")).to.be.undefined;
       expect(el.text()).to.eq("Send Message");
     });
+    cy.get('[data-cy="contact-input-email"]').type("test@example.com{enter}"); //* Key press simulation
     // cy.get('[data-cy="contact-btn-submit"]')
     //   .contains('Send Message')
     //   .should('not.have.attr', 'disabled');
     cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
-    cy.get("@submitBtn").click();
+    // cy.get('@submitBtn').click();
     cy.get("@submitBtn").contains("Sending...");
     cy.get("@submitBtn").should("have.attr", "disabled");
   });
