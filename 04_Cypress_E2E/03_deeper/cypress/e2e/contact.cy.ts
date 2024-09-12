f8be4847670fac368c21091b56694ae4a98da351
Michal Radomski <michalra@op.pl>
2024-09-13 00:55:01 +0200

    Cypress End-to-End Testing course - commit 17

--- 04_Cypress_E2E/03_deeper/cypress/e2e/contact.cy.ts
+++ 04_Cypress_E2E/03_deeper/cypress/e2e/contact.cy.ts
@@ -16,18 +16,39 @@
 // });
 
 //* V2
+// describe("contact form", (): void => {
+//   it("should submit the form", (): void => {
+//     cy.visit("http://localhost:5173/about");
+//     cy.get('[data-cy="contact-input-message"]').type("Hello world!");
+//     cy.get('[data-cy="contact-input-name"]').type("John Doe");
+//     cy.get('[data-cy="contact-input-email"]').type("test@example.com");
+//     cy.get('[data-cy="contact-btn-submit"]').contains("Send Message").should("not.have.attr", "disabled");
+//     cy.get('[data-cy="contact-btn-submit"]').as("submitBtn"); //* Alias - recommended
+//     cy.get("@submitBtn").click();
+//     cy.get("@submitBtn").contains("Sending...");
+//     cy.get("@submitBtn").should("have.attr", "disabled");
+//     // const btn:Cypress.Chainable<JQuery<HTMLElement>> = cy.get('[data-cy="contact-btn-submit"]'); //* Not recommended!
+//     // console.log("btn:", btn);
+//   });
+// });
+
+//* V3
 describe("contact form", (): void => {
   it("should submit the form", (): void => {
     cy.visit("http://localhost:5173/about");
     cy.get('[data-cy="contact-input-message"]').type("Hello world!");
     cy.get('[data-cy="contact-input-name"]').type("John Doe");
     cy.get('[data-cy="contact-input-email"]').type("test@example.com");
-    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message").should("not.have.attr", "disabled");
-    cy.get('[data-cy="contact-btn-submit"]').as("submitBtn"); //* alias
+    cy.get('[data-cy="contact-btn-submit"]').then((el: JQuery<HTMLElement>) => {
+      expect(el.attr("disabled")).to.be.undefined;
+      expect(el.text()).to.eq("Send Message");
+    });
+    // cy.get('[data-cy="contact-btn-submit"]')
+    //   .contains('Send Message')
+    //   .should('not.have.attr', 'disabled');
+    cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
     cy.get("@submitBtn").click();
     cy.get("@submitBtn").contains("Sending...");
     cy.get("@submitBtn").should("have.attr", "disabled");
-    // const btn = cy.get('[data-cy="contact-btn-submit"]');
-    // console.log("btn:", btn);
   });
 });
