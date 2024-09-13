9bfdfc816b102127e76cced08b6adcf4ddcf10cc
Michal Radomski <michalra@op.pl>
2024-09-14 00:24:59 +0200

    Cypress End-to-End Testing course - commit 22

--- 04_Cypress_E2E/03_deeper/cypress.config.ts
+++ 04_Cypress_E2E/03_deeper/cypress.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  // video:true,
+  // screenshotOnRunFailure:true,
+  // videosFolder:"./",
+  // screenshotsFolder: "./",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
