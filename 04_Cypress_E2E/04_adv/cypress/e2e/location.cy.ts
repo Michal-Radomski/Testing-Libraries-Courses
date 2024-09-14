aa60ceb59ed332b23b8befc1f07a40df307cd099
Michal Radomski <michalra@op.pl>
2024-09-14 23:48:37 +0200

    Cypress End-to-End Testing course - commit 34

--- 04_Cypress_E2E/04_adv/cypress/e2e/location.cy.ts
+++ 04_Cypress_E2E/04_adv/cypress/e2e/location.cy.ts
@@ -1,13 +1,21 @@
 /// <reference types="cypress" />
 
-describe("share location", () => {
-  beforeEach(() => {
+interface FakePosition {
+  coords: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
+describe("share location", (): void => {
+  beforeEach((): void => {
+    cy.clock();
     cy.fixture("user-location.json").as("userLocation");
-    cy.visit("/").then((win) => {
+    cy.visit("/").then((win: Cypress.AUTWindow) => {
       cy.get("@userLocation").then((fakePosition) => {
         cy.stub(win.navigator.geolocation, "getCurrentPosition")
           .as("getUserPosition")
-          .callsFake((cb) => {
+          .callsFake((cb: Function) => {
             setTimeout(() => {
               cb(fakePosition);
             }, 100);
@@ -18,20 +26,21 @@ describe("share location", () => {
       cy.spy(win.localStorage, "getItem").as("getStoredLocation");
     });
   });
-  it("should fetch the user location", () => {
+
+  it("should fetch the user location", (): void => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get("@getUserPosition").should("have.been.called");
     cy.get('[data-cy="get-loc-btn"]').should("be.disabled");
     cy.get('[data-cy="actions"]').should("contain", "Location fetched"); // contains()
   });
 
-  it("should share a location URL", () => {
+  it("should share a location URL", (): void => {
     cy.get('[data-cy="name-input"]').type("John Doe");
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('[data-cy="share-loc-btn"]').click();
     cy.get("@saveToClipboard").should("have.been.called");
     cy.get("@userLocation").then((fakePosition) => {
-      const { latitude, longitude } = (fakePosition as any).coords;
+      const { latitude, longitude } = (fakePosition as unknown as FakePosition).coords;
       cy.get("@saveToClipboard").should(
         "have.been.calledWithMatch",
         new RegExp(`${latitude}.*${longitude}.*${encodeURI("John Doe")}`)
@@ -45,5 +54,9 @@ describe("share location", () => {
     cy.get("@storeLocation").should("have.been.called");
     cy.get('[data-cy="share-loc-btn"]').click();
     cy.get("@getStoredLocation").should("have.been.called");
+    cy.get('[data-cy="info-message"]').should("be.visible");
+    cy.get('[data-cy="info-message"]').should("have.class", "visible");
+    cy.tick(2000);
+    cy.get('[data-cy="info-message"]').should("not.be.visible");
   });
 });
