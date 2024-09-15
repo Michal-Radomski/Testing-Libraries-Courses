fe90aacb5f4b3ebca9e92c0f5e7d68772babc386
Michal Radomski <michalra@op.pl>
2024-09-15 22:34:11 +0200

    Cypress End-to-End Testing course - commit 37

--- 04_Cypress_E2E/05_auth/cypress.config.js
+++ 04_Cypress_E2E/05_auth/cypress.config.js
@@ -5,7 +5,7 @@ import { seed } from "./prisma/seed-test";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on, _config) {
       // implement node event listeners here
       on("task", {
         async seedDatabase() {
--- 04_Cypress_E2E/05_auth/cypress/e2e/takeaways.cy.js
+++ 04_Cypress_E2E/05_auth/cypress/e2e/takeaways.cy.js
@@ -1,7 +1,17 @@
 /// <reference types="cypress" />
 
+// describe("Takeaways", () => {
+//   it("should display a list of fetched takeaways", () => {
+//     cy.visit("/");
+//   });
+// });
+
 describe("Takeaways", () => {
+  beforeEach(() => {
+    cy.task("seedDatabase");
+  });
   it("should display a list of fetched takeaways", () => {
     cy.visit("/");
+    cy.get('[data-cy="takeaway-item"]').should("have.length", 2);
   });
 });
