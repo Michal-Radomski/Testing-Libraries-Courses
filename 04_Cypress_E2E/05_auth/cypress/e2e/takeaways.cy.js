effa1f3e49b4d93b84975c23b3c5591da04a9ff0
Michal Radomski <michalra@op.pl>
2024-09-15 23:10:08 +0200

    Cypress End-to-End Testing course - commit 39

--- 04_Cypress_E2E/05_auth/cypress/e2e/takeaways.cy.js
+++ 04_Cypress_E2E/05_auth/cypress/e2e/takeaways.cy.js
@@ -4,6 +4,7 @@ describe("Takeaways", () => {
   beforeEach(() => {
     cy.task("seedDatabase");
   });
+
   it("should display a list of fetched takeaways", () => {
     cy.visit("/");
     cy.get('[data-cy="takeaway-item"]').should("have.length", 2);
