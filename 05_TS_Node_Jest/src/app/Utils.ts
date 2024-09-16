567bcbc8b08c0af66e0a768e91396298270009c5
Michal Radomski <michalra@op.pl>
2024-09-17 00:03:41 +0200

    Unit testing for TS & NodeJs with Jest course - commit 04

--- 05_TS_Node_Jest/src/app/Utils.ts
+++ 05_TS_Node_Jest/src/app/Utils.ts
@@ -10,3 +10,22 @@ export class StringUtils {
 export function toUpperCase(arg: string): string {
   return arg.toUpperCase();
 }
+
+export type stringInfo = {
+  lowerCase: string;
+  upperCase: string;
+  characters: string[];
+  length: number;
+  extraInfo: Object | undefined;
+};
+
+/* istanbul ignore next */
+export function getStringInfo(arg: string): stringInfo {
+  return {
+    lowerCase: arg.toLowerCase(),
+    upperCase: arg.toUpperCase(),
+    characters: Array.from(arg),
+    length: arg.length,
+    extraInfo: {},
+  };
+}
--- 05_TS_Node_Jest/src/test/Utils.test.ts
+++ 05_TS_Node_Jest/src/test/Utils.test.ts
@@ -1,15 +1,32 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", (): void => {
-  it("Should return correct upperCase", (): void => {
+  it("should return uppercase of valid string", (): void => {
     // Arrange
     const sut = toUpperCase;
     const expected = "ABC";
-
     // Act
     const actual = sut("abc");
-
     // Assert
     expect(actual).toBe(expected);
+  });
+
+  it.only("should return info for valid string", (): void => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual(["M", "y", "-", "S", "t", "r", "i", "n", "g"]);
+    expect(actual.characters).toContain<string>("M");
+    expect(actual.characters).toEqual(expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"]));
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
