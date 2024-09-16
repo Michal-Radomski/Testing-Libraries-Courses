35e7ff412aa0f15932d51e4663f6e59e00378370
Michal Radomski <michalra@op.pl>
2024-09-16 23:44:40 +0200

    Unit testing for TS & NodeJs with Jest course - commit 03

--- 05_TS_Node_Jest/src/test/Utils.test.ts
+++ 05_TS_Node_Jest/src/test/Utils.test.ts
@@ -2,7 +2,14 @@ import { toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", (): void => {
   it("Should return correct upperCase", (): void => {
-    const result = toUpperCase("abc");
-    expect(result).toBe("ABC");
+    // Arrange
+    const sut = toUpperCase;
+    const expected = "ABC";
+
+    // Act
+    const actual = sut("abc");
+
+    // Assert
+    expect(actual).toBe(expected);
   });
 });
