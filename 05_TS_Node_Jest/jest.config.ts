9fb930277d64a979f2c246de7a79dc7e49d4e3cc
Michal Radomski <michalra@op.pl>
2024-09-18 02:06:36 +0200

    Unit testing for TS & NodeJs with Jest course - commit 14

--- 05_TS_Node_Jest/jest.config.ts
+++ 05_TS_Node_Jest/jest.config.ts
@@ -1,12 +1,16 @@
 import type { Config } from "@jest/types";
 
+const baseDir = "<rootDir>/src/app/pass_checker";
+const baseTestDir = "<rootDir>/src/test/pass_checker";
+
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
   testTimeout: 10000,
   collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/app/**/*.ts"],
+  collectCoverageFrom: [`${baseDir}/**/*.ts`],
+  testMatch: [`${baseTestDir}/**/*.ts`],
 };
 
 export default config;
