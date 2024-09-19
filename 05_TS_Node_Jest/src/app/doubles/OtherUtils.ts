73db7ea072f1b5c5cad2fb78df44dc80e41d5038
Michal Radomski <michalra@op.pl>
2024-09-20 01:04:23 +0200

    Unit testing for TS & NodeJs with Jest course - commit 28

--- 05_TS_Node_Jest/src/app/doubles/OtherUtils.ts
+++ 05_TS_Node_Jest/src/app/doubles/OtherUtils.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+
 export type StringInfo = {
   lowerCase: string;
   upperCase: string;
@@ -7,6 +9,14 @@ export type StringInfo = {
 };
 
 type LoggerServiceCallBack = (arg: string) => void;
+
+export function toUpperCase(arg: string): string {
+  return arg.toUpperCase();
+}
+
+export function toLowerCaseWithId(arg: string): string {
+  return arg.toLowerCase() + uuidv4();
+}
 
 export function calculateComplexity(stringInfo: StringInfo): number {
   return Object.keys(stringInfo.extraInfo!).length * stringInfo.length;
