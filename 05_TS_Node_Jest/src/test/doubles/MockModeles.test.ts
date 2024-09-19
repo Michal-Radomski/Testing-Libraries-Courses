b4a1a0925894d8485bf53f6a6e8225747f9cfd15
Michal Radomski <michalra@op.pl>
2024-09-20 01:39:42 +0200

    Unit testing for TS & NodeJs with Jest course - commit 29

---
+++ 05_TS_Node_Jest/src/test/doubles/MockModeles.test.ts
@@ -0,0 +1,35 @@
+jest.mock("../../app/doubles/OtherUtils", () => ({
+  ...jest.requireActual("../../app/doubles/OtherUtils"),
+  calculateComplexity: () => {
+    return 10;
+  },
+}));
+
+jest.mock("uuid", () => ({
+  v4: () => "123",
+}));
+
+// jest.mock("uuid", () => {
+//   return {
+//     v4: () => "123",
+//   };
+// });
+
+import * as OtherUtils from "../../app/doubles/OtherUtils";
+
+describe("module tests", (): void => {
+  test("calculate complexity", (): void => {
+    const result = OtherUtils.calculateComplexity({} as any);
+    expect(result).toBe(10);
+  });
+
+  test("keep other functions", (): void => {
+    const result = OtherUtils.toUpperCase("abc");
+    expect(result).toBe("ABC");
+  });
+
+  test("string with id", (): void => {
+    const result = OtherUtils.toLowerCaseWithId("ABC");
+    expect(result).toBe("abc123");
+  });
+});
