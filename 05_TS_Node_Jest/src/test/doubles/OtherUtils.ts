f24449727404e8041bde41efaf44d1124620a5b3
Michal Radomski <michalra@op.pl>
2024-09-20 00:41:26 +0200

    Unit testing for TS & NodeJs with Jest course - commit 22

--- 05_TS_Node_Jest/src/test/doubles/OtherUtils.ts
+++ 05_TS_Node_Jest/src/test/doubles/OtherUtils.ts
@@ -1,7 +1,39 @@
 import { calculateComplexity, StringInfo, toUpperCaseWithCb } from "../../app/doubles/OtherUtils";
 
 describe("OtherUtils test suite", (): void => {
+  //* Mocks
+  describe.only("Tracking callbacks", (): void => {
+    let cbArgs = [] as string[];
+    let timesCalled = 0;
+
+    function callBackMock(arg: string): void {
+      cbArgs.push(arg);
+      timesCalled++;
+    }
+
+    afterEach((): void => {
+      // clearing tracking fields:
+      cbArgs = [];
+      timesCalled = 0;
+    });
+
+    it("calls callback for invalid argument - track calls", (): void => {
+      const actual = toUpperCaseWithCb("", callBackMock);
+      expect(actual).toBeUndefined();
+      expect(cbArgs).toContain("Invalid argument!");
+      expect(timesCalled).toBe(1);
+    });
+
+    it("calls callback for valid argument - track calls", (): void => {
+      const actual = toUpperCaseWithCb("abc", callBackMock);
+      expect(actual).toBe("ABC");
+      expect(cbArgs).toContain("called function with abc");
+      expect(timesCalled).toBe(1);
+    });
+  });
+
   it("ToUpperCase - calls callback for invalid argument", (): void => {
+    //* Fake
     const actual = toUpperCaseWithCb("", () => {});
     expect(actual).toBeUndefined();
   });
@@ -12,6 +44,7 @@ describe("OtherUtils test suite", (): void => {
   });
 
   it("Calculates complexity", (): void => {
+    //* Stub
     const someInfo = {
       length: 5,
       extraInfo: {
