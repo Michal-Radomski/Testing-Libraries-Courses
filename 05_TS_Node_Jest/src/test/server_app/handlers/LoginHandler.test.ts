3b1a2f65f823a7fad46cd8be168ee656246fafd0
Michal Radomski <michalra@op.pl>
2024-09-21 01:21:59 +0200

    Unit testing for TS & NodeJs with Jest course - commit 45

--- 05_TS_Node_Jest/src/test/server_app/handlers/LoginHandler.test.ts
+++ 05_TS_Node_Jest/src/test/server_app/handlers/LoginHandler.test.ts
@@ -47,52 +47,52 @@ describe("LoginHandler test suite", (): void => {
     jest.clearAllMocks();
   });
 
-  it("should return token for valid accounts in requests", async () => {
+  it("should return token for valid accounts in requests", async (): Promise<void> => {
     request.method = HTTP_METHODS.POST;
     getRequestBodyMock.mockResolvedValueOnce(someAccount);
     authorizerMock.login.mockResolvedValueOnce(someToken);
 
     await sut.handleRequest();
 
-    expect(authorizerMock.login).toBeCalledWith(someAccount.userName, someAccount.password);
+    expect(authorizerMock.login).toHaveBeenCalledWith(someAccount.userName, someAccount.password);
     expect(responseMock.statusCode).toBe(HTTP_CODES.CREATED);
-    expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.CREATED, { "Content-Type": "application/json" });
-    expect(responseMock.write).toBeCalledWith(
+    expect(responseMock.writeHead).toHaveBeenCalledWith(HTTP_CODES.CREATED, { "Content-Type": "application/json" });
+    expect(responseMock.write).toHaveBeenCalledWith(
       JSON.stringify({
         token: someToken,
       })
     );
   });
 
-  it("should return not found for invalid accounts in requests", async () => {
+  it("should return not found for invalid accounts in requests", async (): Promise<void> => {
     request.method = HTTP_METHODS.POST;
     getRequestBodyMock.mockResolvedValueOnce(someAccount);
     authorizerMock.login.mockResolvedValueOnce(undefined);
 
     await sut.handleRequest();
 
-    expect(authorizerMock.login).toBeCalledWith(someAccount.userName, someAccount.password);
+    expect(authorizerMock.login).toHaveBeenCalledWith(someAccount.userName, someAccount.password);
     expect(responseMock.statusCode).toBe(HTTP_CODES.NOT_fOUND);
-    expect(responseMock.write).toBeCalledWith(JSON.stringify("wrong username or password"));
+    expect(responseMock.write).toHaveBeenCalledWith(JSON.stringify("wrong username or password"));
   });
 
-  it("should return bad request for invalid requests", async () => {
+  it("should return bad request for invalid requests", async (): Promise<void> => {
     request.method = HTTP_METHODS.POST;
     getRequestBodyMock.mockResolvedValueOnce({});
 
     await sut.handleRequest();
 
-    expect(authorizerMock.login).not.toBeCalled();
+    expect(authorizerMock.login).not.toHaveBeenCalled();
     expect(responseMock.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
-    expect(responseMock.write).toBeCalledWith(JSON.stringify("userName and password required"));
+    expect(responseMock.write).toHaveBeenCalledWith(JSON.stringify("userName and password required"));
   });
 
-  it("should do nothing for not supported http methods", async () => {
+  it("should do nothing for not supported http methods", async (): Promise<void> => {
     request.method = HTTP_METHODS.GET;
     await sut.handleRequest();
 
-    expect(responseMock.writeHead).not.toBeCalled();
-    expect(responseMock.write).not.toBeCalled();
-    expect(getRequestBodyMock).not.toBeCalled();
+    expect(responseMock.writeHead).not.toHaveBeenCalled();
+    expect(responseMock.write).not.toHaveBeenCalled();
+    expect(getRequestBodyMock).not.toHaveBeenCalled();
   });
 });
