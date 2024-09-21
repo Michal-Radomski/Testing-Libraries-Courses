9101fd12f2b93c4065e6a4dadcab9fc582db5755
Michal Radomski <michalra@op.pl>
2024-09-21 23:46:28 +0200

    Unit testing for TS & NodeJs with Jest course - commit 55

--- 05_TS_Node_Jest/src/test/server_app/data/IdGenerator.test.ts
+++ 05_TS_Node_Jest/src/test/server_app/data/IdGenerator.test.ts
@@ -3,6 +3,7 @@ import { generateRandomId } from "../../../app/server_app/data/IdGenerator";
 describe("IdGenerator test suite", (): void => {
   it("should return a random string", (): void => {
     const randomId: string = generateRandomId();
+    // console.log("randomId:", randomId);
     expect(randomId.length).toBe(20);
   });
 });
