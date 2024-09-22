f18dbc38b3bd2fb7bedb775d398b79d57f5da117
Michal Radomski <michalra@op.pl>
2024-09-22 21:44:54 +0200

    Unit testing for TS & NodeJs with Jest course - commit 66

--- 05_TS_Node_Jest/jest.config.ts
+++ 05_TS_Node_Jest/jest.config.ts
@@ -15,7 +15,8 @@ import type { Config } from "@jest/types";
 const baseDir = "<rootDir>/src/app/server_app";
 // const baseTestDir = "<rootDir>/src/test/server_app";
 // const baseTestDir = "<rootDir>/src/test/server_app2";
-const baseTestDir = "<rootDir>/src/test/server_app3";
+// const baseTestDir = "<rootDir>/src/test/server_app3";
+const baseTestDir = "<rootDir>/src/test";
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
@@ -27,6 +28,8 @@ const config: Config.InitialOptions = {
   testMatch: [
     // `${baseTestDir}/**/*.ts`,
     `${baseTestDir}/**/*test.ts`,
+    `${baseTestDir}/server_app/**/*test.ts`,
+    `${baseTestDir}/server_app2/**/*test.ts`,
   ],
   setupFiles: ["<rootDir>/src/test/server_app3/utils/config.ts"],
 };
