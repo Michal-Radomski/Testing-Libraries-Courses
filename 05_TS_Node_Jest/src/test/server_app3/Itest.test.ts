7497ee6c814bb4e5d3a878bb899e77c6f39984d7
Michal Radomski <michalra@op.pl>
2024-09-22 12:55:53 +0200

    Unit testing for TS & NodeJs with Jest course - commit 60

---
+++ 05_TS_Node_Jest/src/test/server_app3/Itest.test.ts
@@ -0,0 +1,49 @@
+import { Account } from "../../app/server_app/model/AuthModel";
+import { HTTP_CODES, HTTP_METHODS } from "../../app/server_app/model/ServerModel";
+import { Server } from "../../app/server_app/server/Server";
+import { makeAwesomeRequest } from "./utils/http-client";
+
+describe("Server app integration tests", (): void => {
+  let server: Server;
+
+  beforeAll((): void => {
+    server = new Server();
+    server.startServer();
+  });
+
+  afterAll((): void => {
+    server.stopServer();
+  });
+
+  const someUser: Account = {
+    id: "",
+    userName: "someUserName",
+    password: "somePassword",
+  };
+
+  it("should register new user", async (): Promise<void> => {
+    const result = await fetch("http://localhost:8080/register", {
+      method: HTTP_METHODS.POST,
+      body: JSON.stringify(someUser),
+    });
+    const resultBody = await result.json();
+
+    expect(result.status).toBe(HTTP_CODES.CREATED);
+    expect(resultBody.userId).toBeDefined();
+  });
+
+  it("should register new user with awesomeRequest", async (): Promise<void> => {
+    const result = await makeAwesomeRequest(
+      {
+        host: "localhost",
+        port: 8080,
+        method: HTTP_METHODS.POST,
+        path: "/register",
+      },
+      someUser
+    );
+
+    expect(result.statusCode).toBe(HTTP_CODES.CREATED);
+    expect(result.body.userId).toBeDefined();
+  });
+});
