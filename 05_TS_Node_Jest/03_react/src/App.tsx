baceb8fb687a5d4f15822e81d040eb4335973745
Michal Radomski <michalra@op.pl>
2024-09-23 00:00:28 +0200

    Unit testing for TS & NodeJs with Jest course - commit 77

--- 05_TS_Node_Jest/03_react/src/App.tsx
+++ 05_TS_Node_Jest/03_react/src/App.tsx
@@ -2,8 +2,51 @@ import React from "react";
 
 import "./App.scss";
 
+type ChildProps = {
+  parentCounter: number;
+};
+
+function ChildCounter(props: ChildProps): JSX.Element {
+  const [childCounter, setChildCounter] = React.useState<number>(0);
+
+  function handleClickIncrement(): void {
+    setChildCounter(childCounter + 1);
+  }
+  console.log("Rendering child component");
+
+  return (
+    <div style={{ border: "1px dashed red" }}>
+      <button onClick={handleClickIncrement}>Increment child counter</button>
+      <p>ChildCounter: {childCounter}</p>
+      <p>ParentCounter: {props.parentCounter}</p>
+    </div>
+  );
+}
+
+function ParentCounter(): JSX.Element {
+  const [parentCounter, setParentCounter] = React.useState<number>(0);
+
+  function handleClickIncrement(): void {
+    setParentCounter(parentCounter + 1);
+  }
+  console.log("Rendering parent component");
+
+  return (
+    <div style={{ border: "1px dashed blue" }}>
+      <button onClick={handleClickIncrement}>Increment parent counter</button>
+      <ChildCounter parentCounter={parentCounter} />
+    </div>
+  );
+}
+
 const App = (): JSX.Element => {
-  return <React.Fragment>App</React.Fragment>;
+  return (
+    <React.Fragment>
+      App
+      <br />
+      <ParentCounter />
+    </React.Fragment>
+  );
 };
 
 export default App;
