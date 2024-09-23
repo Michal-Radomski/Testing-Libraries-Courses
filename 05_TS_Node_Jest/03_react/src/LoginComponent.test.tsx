a2b43c18d095a2ee4307575b5a4bd1a9564ccdcb
Michal Radomski <michalra@op.pl>
2024-09-23 22:14:11 +0200

    Unit testing for TS & NodeJs with Jest course - commit 80

--- 05_TS_Node_Jest/03_react/src/LoginComponent.test.tsx
+++ 05_TS_Node_Jest/03_react/src/LoginComponent.test.tsx
@@ -8,13 +8,48 @@ describe("Login component tests", (): void => {
   };
   const setTokenMock = jest.fn() as jest.Mock<any, any>;
 
-  it("should render correctly the login component", (): void => {
-    const container = render(<LoginComponent loginService={loginServiceMock} setToken={setTokenMock} />)
+  // it("should render correctly the login component", (): void => {
+  //   const container = render(<LoginComponent loginService={loginServiceMock} setToken={setTokenMock} />)
+  //     .container as HTMLDivElement;
+  //   // console.log("container.innerHTML:", container.innerHTML);
+  //   expect(container).toBeInTheDocument();
+
+  //   const mainElement = screen.getByRole("main") as HTMLDivElement;
+  //   expect(mainElement).toBeInTheDocument();
+  // });
+
+  let container: HTMLDivElement;
+
+  function setup(): void {
+    container = render(<LoginComponent loginService={loginServiceMock} setToken={setTokenMock} />)
       .container as HTMLDivElement;
     // console.log("container.innerHTML:", container.innerHTML);
-    expect(container).toBeInTheDocument();
+  }
 
+  beforeEach((): void => {
+    setup();
+  });
+
+  it("should render correctly the login component", (): void => {
     const mainElement = screen.getByRole("main") as HTMLDivElement;
     expect(mainElement).toBeInTheDocument();
+    expect(screen.queryByTestId("resultLabel")).not.toBeInTheDocument();
+  });
+
+  it("should render correctly - query by test id", (): void => {
+    const inputs = screen.getAllByTestId("input") as HTMLInputElement[]; //* data-testid="input"
+    expect(inputs).toHaveLength(3);
+    expect(inputs[0].getAttribute("value")).toBe("");
+    expect(inputs[1].getAttribute("value")).toBe("");
+    expect(inputs[2].getAttribute("value")).toBe("Login");
+  });
+
+  it("should render correctly - query by document query", (): void => {
+    // eslint-disable-next-line testing-library/no-node-access
+    const inputs = container.querySelectorAll("input") as NodeListOf<HTMLInputElement>; //* Error
+    expect(inputs).toHaveLength(3);
+    expect(inputs[0].value).toBe("");
+    expect(inputs[1].value).toBe("");
+    expect(inputs[2].value).toBe("Login");
   });
 });
